cb25540de2230249449f7d26fe115ea97d1d927e
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-18 21:52:02 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require 'pry'
+
+
+class School
+
+  attr_accessor :name, :roster
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+  def add_student(student_name, grade)
+     @roster[grade] = [] unless @roster.keys.include?(grade)
+     @roster[grade] << student_name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    # sorted_students = {}
+    # @roster.each do |grade, students| #grade
+    #   sorted_students[grade] = students.sort
+    #   binding.pry
+    # end
+    #   sorted_students
+    @roster.sort.to_h
+    # binding.pry
+  end
+
+end
